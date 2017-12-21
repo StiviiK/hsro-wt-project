cd93d318a0667ed5833be62f24a4e22150640b83
FatLoki <M.Dietl@gmx.net>
2017-12-21 08:33:55 +0100

    Started with enterpage

--- Frontend/src/app/components/enterpage/enterpage.component.ts
+++ Frontend/src/app/components/enterpage/enterpage.component.ts
@@ -5,7 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './enterpage.component.html',
   styleUrls: ['./enterpage.component.scss']
 })
-export class EnterpageComponent implements OnInit {
+export class EnterpageComponent implements OnInit { // fill this with backend later
+  hotTopics = [
+    {
+      name: 'Angular 4 is not working',
+      updated: new Date('1/1/16'),
+      answers: 5,
+    },
+    {
+      name: 'PC doesnt start, help please',
+      updated: new Date('1/17/16'),
+      answers: 1,
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+      answers: 20,
+    }
+  ];
+  lastTopics = [
+    {
+      name: 'How do i do this computing thing',
+      updated: new Date('2/20/16'),
+      answers: 10,
+    },
+    {
+      name: 'Website Remodel',
+      updated: new Date('1/18/16'),
+      answers: 60,
+    }
+  ];
 
   constructor() { }
 
