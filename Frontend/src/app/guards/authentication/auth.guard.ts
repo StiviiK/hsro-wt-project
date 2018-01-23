82de0b2db01e8dadbf6d259dc1f96e4eb83222f9
StiviiK <stefan.kuerzeder@gmail.com>
2018-01-23 15:19:46 +0100

    AuthGuad now verifys the token

--- Frontend/src/app/guards/authentication/auth.guard.ts
+++ Frontend/src/app/guards/authentication/auth.guard.ts
@@ -2,18 +2,39 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { AuthenticatedUser } from '../../models/user/User';
 import { ApiService } from '../../services/api/api.service';
+import { ApiResponse } from '../../models/interfaces/api/ApiResponse';
+import { of } from 'rxjs/observable/of';
+import { AuthenticationService } from '../../services/authentication/authentication';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-
     constructor(private _router: Router,
-                private _api: ApiService) { }
+                private _api: ApiService,
+                private _authService: AuthenticationService) { }
 
     canActivate() {
       if (AuthenticatedUser.load() != null) {
-        return true;
+        return this._api.get<ApiResponse>("Verify")
+          .map(
+            (response: ApiResponse): boolean => {
+              if (response && response.status === true) {
+                return true;
+              }
+
+              this._authService.logout();
+              this._router.navigate(['/login']);
+              return false;
+            }
+          )
+          .catch(() => {
+            this._authService.logout();
+            this._router.navigate(['/login']);
+            return of(false);
+          })
+      } else {
+        this._authService.logout();
+        this._router.navigate(['/login']);
+        return of(false);
       }
-      this._router.navigate(['/login']);
-      return false;
     }
 }
